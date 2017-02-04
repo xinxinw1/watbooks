a5f236a02e2722cee44052d7b1163030eb4f802a
ksinder <kennethsinder@outlook.com>
2017-02-04 18:47:58 -0500

    Added book and course models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Textbook(models.Model):
+    """
+    Represents a single textbook item.
+    """
+
+    name = models.CharField(max_length=120)
+    author = models.CharField(max_length=100)
+    sku = models.IntegerField()
+    new_price = models.DecimalField(decimal_places=2, max_digits=10)
+    used_price = models.DecimalField(decimal_places=2, max_digits=10)
+    is_required = models.BooleanField()
+
+    def __str__(self):
+        return "{0}: {1}".format(self.author, self.name)
+
+    class Meta:
+        verbose_name_plural = "Textbooks"
+        ordering = ["name"]
+
+class Course(models.Model):
+    """
+    Represents a single UW course.
+    """
+
+    subject = models.CharField(max_length=5)
+    catalog_number = models.IntegerField()
+    books = models.ManyToManyField(Textbook)
+
+    def __str__(self):
+        return "{0} {1}".format(self.subject, self.catalog_number)
+
+    class Meta:
+        verbose_name_plural = "Courses"
+        ordering = ["catalog_number"]
